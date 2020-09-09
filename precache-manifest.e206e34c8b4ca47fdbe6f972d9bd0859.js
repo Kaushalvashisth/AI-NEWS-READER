6de280db69ff4929f93e3e100a57bd156f30d66f
KAUSHAL VASHISTH <backspacekaushal@gmail.com>
2020-09-09 18:00:08 +0530

    Updates

---
+++ precache-manifest.e206e34c8b4ca47fdbe6f972d9bd0859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379ad0c9e7dd46fc85aa3c4d0976ee4c",
+    "url": "/AI-NEWS-READER/index.html"
+  },
+  {
+    "revision": "088e6de4da66e446b575",
+    "url": "/AI-NEWS-READER/static/css/main.8ff82754.chunk.css"
+  },
+  {
+    "revision": "3026594e6ac31f2ded4d",
+    "url": "/AI-NEWS-READER/static/js/2.f390fee0.chunk.js"
+  },
+  {
+    "revision": "b95d735f9bdac369c512281af49352ca",
+    "url": "/AI-NEWS-READER/static/js/2.f390fee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088e6de4da66e446b575",
+    "url": "/AI-NEWS-READER/static/js/main.2d7660e0.chunk.js"
+  },
+  {
+    "revision": "6073bd73f92fb4b9f6f7",
+    "url": "/AI-NEWS-READER/static/js/runtime-main.f9b4403f.js"
+  }
+]);
